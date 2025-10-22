5105d0beb3e4b5030507a469034bc6c55b5dac15
mariovagomarzal <mariovagomarzal@gmail.com>
2025-10-22 05:07:06 +0200

    feat: add an instance of single-sorted category for monoids

---
+++ InfinityCategories/HigherCategoryTheory/Monoid.lean
@@ -0,0 +1,36 @@
+/-
+Copyright (c) 2025 Mario Vago Marzal. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Enric Cosme Llópez, Raul Ruiz Mora, Mario Vago Marzal
+-/
+import Mathlib.Algebra.Group.Defs
+import InfinityCategories.HigherCategoryTheory.SingleSortedCategory.Basic
+
+/-!
+TODO: Document the file.
+-/
+
+universe u
+
+namespace Monoid
+
+open HigherCategoryTheory
+
+instance singleSortedCategoryMonoid (M : Type u) [Monoid M] : SingleSortedCategory M where
+  Sc _ _ := 1
+  Tg _ _ := 1
+  PComp _ b a := ⟨_, fun _ ↦ b * a⟩
+  pcomp_dom := by
+    intros
+    apply Iff.intro
+    · intros
+      rfl
+    · intro h
+      simp
+      simp at h
+      exact h
+  comp_sc_is_id := mul_one _
+  comp_tg_is_id := one_mul _
+  assoc _ _ := (mul_assoc _ _ _).symm
+
+end Monoid
