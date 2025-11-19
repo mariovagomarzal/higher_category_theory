b21bf0f42af709991cca4d1ffed4dd311c9e751b
mariovagomarzal <mariovagomarzal@gmail.com>
2025-11-19 10:33:54 +0100

    feat: improve source and target definitions of the underlying category

--- HigherCategoryTheory/HigherCategoryTheory/SingleSortedCategory/Cat/Underlying.lean
+++ HigherCategoryTheory/HigherCategoryTheory/SingleSortedCategory/Cat/Underlying.lean
@@ -21,19 +21,11 @@ def toUnderlyingSingleSortedMCategory {n : ℕ} {Obj : Type u}
     (nS : SingleSortedNCategory n Obj)
     (m : Fin n) :
     SingleSortedNCategory m (cell Obj m) where
-  Sc k f := by
-    simp
-    have k_lt_n : k < n := Nat.lt_trans k.isLt m.isLt
-    let sc_k_f : Obj := nS.Sc ⟨k, k_lt_n⟩ f
-    use sc_k_f
-    exact nS.sck_scj_is_scj f k.isLt
-  Tg k f := by
-    rw [cell_eq_cell_tg]
-    simp
-    have k_lt_n : k < n := Nat.lt_trans k.isLt m.isLt
-    let tg_k_f : Obj := nS.Tg ⟨k, k_lt_n⟩ f
-    use tg_k_f
-    exact nS.tgk_tgj_is_tgj f k.isLt
+  Sc k f := ⟨nS.Sc ⟨k, Nat.lt_trans k.isLt m.isLt⟩ f, by
+    exact nS.sck_scj_is_scj f k.isLt⟩
+  Tg k f := ⟨nS.Tg ⟨k, Nat.lt_trans k.isLt m.isLt⟩ f, by
+    apply (nS.id_sc_iff_id_tg m (nS.Tg ⟨k, Nat.lt_trans k.isLt m.isLt⟩ f)).mpr
+    exact nS.tgk_tgj_is_tgj f k.isLt⟩
   PComp k g f :=
     {
       Dom := by
@@ -50,8 +42,11 @@ def toUnderlyingSingleSortedMCategory {n : ℕ} {Obj : Type u}
           _ = _ := nS.sck_compj_is_compj_sck k.isLt nS_comp_gf
           _ = pcomp_k_gf := congr_pcomp g.property f.property _ _
     }
+  pcomp_dom := by
+    intros
+    rw [Subtype.mk.injEq]
+    exact nS.pcomp_dom
   -- TODO: Prove remaining axioms
-  pcomp_dom := by sorry
   sc_sc_is_sc := by sorry
   tg_sc_is_sc := by sorry
   sc_tg_is_tg := by sorry
@@ -60,8 +55,6 @@ def toUnderlyingSingleSortedMCategory {n : ℕ} {Obj : Type u}
   tg_comp_is_tg := by sorry
   comp_sc_is_id := by sorry
   comp_tg_is_id := by sorry
-  compr_assoc := by sorry
-  compl_assoc := by sorry
   assoc := by sorry
   sck_scj_is_scj := by sorry
   scj_sck_is_scj := by sorry
@@ -69,12 +62,8 @@ def toUnderlyingSingleSortedMCategory {n : ℕ} {Obj : Type u}
   tgk_tgj_is_tgj := by sorry
   tgj_tgk_is_tgj := by sorry
   tgj_sck_is_tgj := by sorry
-  comp_j_sc := by sorry
-  comp_j_tg := by sorry
   sck_compj_is_compj_sck := by sorry
   tgk_compj_is_compj_tgk := by sorry
-  comp_k_exchange := by sorry
-  comp_j_exchange := by sorry
   exchange := by sorry
 
 end SingleSortedNCategory
