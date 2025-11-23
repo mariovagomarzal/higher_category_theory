a907a000cfd5ac2bd674d7eb5bef43e0ebb913f2
mariovagomarzal <mariovagomarzal@gmail.com>
2025-11-23 17:48:56 +0100

    feat: implement a custom tactic for the underlying category proofs

--- HigherCategoryTheory/HigherCategoryTheory/SingleSortedCategory/Cat/Underlying.lean
+++ HigherCategoryTheory/HigherCategoryTheory/SingleSortedCategory/Cat/Underlying.lean
@@ -16,6 +16,9 @@ namespace HigherCategoryTheory
 
 namespace SingleSortedNCategory
 
+macro (name := inherit_axiom) "inherit_axiom" axiom_name:ident : tactic =>
+  `(tactic| (intros; simp; apply $axiom_name <;> (simp at *; omega)))
+
 open SingleSortedCategoryFamily in
 def toUnderlyingSingleSortedMCategory {n : ℕ} {Obj : Type u}
     (nS : SingleSortedNCategory n Obj)
@@ -40,40 +43,13 @@ def toUnderlyingSingleSortedMCategory {n : ℕ} {Obj : Type u}
           _ = _ := nS.sck_compj_is_compj_sck k.isLt nS_comp_gf
           _ = pcomp_k_gf := congr_pcomp g.property f.property _ _
     }
-  pcomp_dom := by
-    intros
-    rw [Subtype.mk.injEq]
-    exact nS.pcomp_dom
-  sc_sc_is_sc := by
-    intros
-    rw [Subtype.mk.injEq]
-    exact nS.sc_sc_is_sc _ _
-  tg_sc_is_sc := by
-    intros
-    rw [Subtype.mk.injEq]
-    exact nS.tg_sc_is_sc _ _
-  sc_tg_is_tg := by
-    intros
-    rw [Subtype.mk.injEq]
-    exact nS.sc_tg_is_tg _ _
-  tg_tg_is_tg := by
-    intros
-    rw [Subtype.mk.injEq]
-    exact nS.tg_tg_is_tg _ _
-  sc_comp_is_sc := by
-    intro k g f comp_gf
-    rw [Subtype.mk.injEq]
-    apply nS.sc_comp_is_sc
-    simp only [sc_is_tg] at *
-    rw [Subtype.mk.injEq] at *
-    exact comp_gf
-  tg_comp_is_tg := by
-    intro k g f comp_gf
-    rw [Subtype.mk.injEq]
-    apply nS.tg_comp_is_tg
-    simp only [sc_is_tg] at *
-    rw [Subtype.mk.injEq] at *
-    exact comp_gf
+  pcomp_dom := by inherit_axiom nS.pcomp_dom
+  sc_sc_is_sc := by inherit_axiom nS.sc_sc_is_sc
+  tg_sc_is_sc := by inherit_axiom nS.tg_sc_is_sc
+  sc_tg_is_tg := by inherit_axiom nS.sc_tg_is_tg
+  tg_tg_is_tg := by inherit_axiom nS.tg_tg_is_tg
+  sc_comp_is_sc := by inherit_axiom nS.sc_comp_is_sc
+  tg_comp_is_tg := by inherit_axiom nS.tg_comp_is_tg
   comp_sc_is_id := by
     intros
     rw [Subtype.mk.injEq]
@@ -82,80 +58,16 @@ def toUnderlyingSingleSortedMCategory {n : ℕ} {Obj : Type u}
     intros
     rw [Subtype.mk.injEq]
     exact nS.comp_tg_is_id _ _
-  assoc := by
-    intro k f g h comp_fg comp_gh
-    rw [Subtype.mk.injEq]
-    apply nS.assoc
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_fg
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_gh
-  sck_scj_is_scj := by
-    intros k j f j_lt_k
-    rw [Subtype.mk.injEq]
-    apply nS.sck_scj_is_scj
-    exact j_lt_k
-  scj_sck_is_scj := by
-    intros k j f j_lt_k
-    rw [Subtype.mk.injEq]
-    apply nS.scj_sck_is_scj
-    exact j_lt_k
-  scj_tgk_is_scj := by
-    intros k j f j_lt_k
-    rw [Subtype.mk.injEq]
-    apply nS.scj_tgk_is_scj
-    exact j_lt_k
-  tgk_tgj_is_tgj := by
-    intros k j f j_lt_k
-    rw [Subtype.mk.injEq]
-    apply nS.tgk_tgj_is_tgj
-    exact j_lt_k
-  tgj_tgk_is_tgj := by
-    intros k j f j_lt_k
-    rw [Subtype.mk.injEq]
-    apply nS.tgj_tgk_is_tgj
-    exact j_lt_k
-  tgj_sck_is_tgj := by
-    intros k j f j_lt_k
-    rw [Subtype.mk.injEq]
-    apply nS.tgj_sck_is_tgj
-    exact j_lt_k
-  sck_compj_is_compj_sck := by
-    intros k j f g j_lt_k comp_j_gf
-    rw [Subtype.mk.injEq]
-    apply nS.sck_compj_is_compj_sck
-    · exact j_lt_k
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_j_gf
-  tgk_compj_is_compj_tgk := by
-    intros k j f g j_lt_k comp_j_gf
-    rw [Subtype.mk.injEq]
-    apply nS.tgk_compj_is_compj_tgk
-    · exact j_lt_k
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_j_gf
-  exchange := by
-    intro k j f₁ f₂ g₁ g₂ j_lt_k
-      comp_k_g₂g₁ comp_k_f₂f₁ comp_j_g₂f₂ comp_j_g₁f₁
-    rw [Subtype.mk.injEq]
-    apply nS.exchange
-    · exact j_lt_k
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_k_g₂g₁
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_k_f₂f₁
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_j_g₂f₂
-    · simp only [sc_is_tg] at *
-      rw [Subtype.mk.injEq] at *
-      exact comp_j_g₁f₁
+  assoc := by inherit_axiom nS.assoc
+  sck_scj_is_scj := by inherit_axiom nS.sck_scj_is_scj
+  scj_sck_is_scj := by inherit_axiom nS.scj_sck_is_scj
+  scj_tgk_is_scj := by inherit_axiom nS.scj_tgk_is_scj
+  tgk_tgj_is_tgj := by inherit_axiom nS.tgk_tgj_is_tgj
+  tgj_tgk_is_tgj := by inherit_axiom nS.tgj_tgk_is_tgj
+  tgj_sck_is_tgj := by inherit_axiom nS.tgj_sck_is_tgj
+  sck_compj_is_compj_sck := by inherit_axiom nS.sck_compj_is_compj_sck
+  tgk_compj_is_compj_tgk := by inherit_axiom nS.tgk_compj_is_compj_tgk
+  exchange := by inherit_axiom nS.exchange
 
 end SingleSortedNCategory
 
