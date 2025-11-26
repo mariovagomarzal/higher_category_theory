3fce69c11eddd4826a2ba6f4d7fa8919369ac2ba
mariovagomarzal <mariovagomarzal@gmail.com>
2025-11-26 11:13:25 +0100

    feat: implement the underlying definitions for the omega case

--- HigherCategoryTheory/SingleSorted/Underlying.lean
+++ HigherCategoryTheory/SingleSorted/Underlying.lean
@@ -44,10 +44,53 @@ def SingleSortedNCategory.underlying (S : SingleSortedNCategory n obj) (m : Fin 
           _ = _ := S.sck_compj_eq_compj_sck k.isLt (S.pcomp_dom.mp dom)
           _ = comp_gf := by apply congr_comp₁ f.property g.property⟩
   }
-  pcomp_dom := by
+  pcomp_dom := by inherit_axiom S.pcomp_dom
+  sck_sck_eq_sck := by inherit_axiom S.sck_sck_eq_sck
+  tgk_sck_eq_sck := by inherit_axiom S.tgk_sck_eq_sck
+  sck_tgk_eq_tgk := by inherit_axiom S.sck_tgk_eq_tgk
+  tgk_tgk_eq_tgk := by inherit_axiom S.tgk_tgk_eq_tgk
+  sck_compk_eq_sck := by inherit_axiom S.sck_compk_eq_sck
+  tgk_compk_eq_tgk := by inherit_axiom S.tgk_compk_eq_tgk
+  compk_sck_eq_id := by
     intros
-    simp
-    apply S.pcomp_dom
+    rw [Subtype.mk.injEq]
+    apply S.compk_sck_eq_id
+  compk_tgk_eq_id := by
+    intros
+    rw [Subtype.mk.injEq]
+    apply S.compk_tgk_eq_id
+  assoc := by inherit_axiom S.assoc
+  sck_scj_eq_scj := by inherit_axiom S.sck_scj_eq_scj
+  scj_sck_eq_scj := by inherit_axiom S.scj_sck_eq_scj
+  scj_tgk_eq_scj := by inherit_axiom S.scj_tgk_eq_scj
+  tgk_tgj_eq_tgj := by inherit_axiom S.tgk_tgj_eq_tgj
+  tgj_tgk_eq_tgj := by inherit_axiom S.tgj_tgk_eq_tgj
+  tgj_sck_eq_tgj := by inherit_axiom S.tgj_sck_eq_tgj
+  sck_compj_eq_compj_sck := by inherit_axiom S.sck_compj_eq_compj_sck
+  tgk_compj_eq_compj_tgk := by inherit_axiom S.tgk_compj_eq_compj_tgk
+  exchange := by inherit_axiom S.exchange
+
+/-- TODO: Comment. -/
+@[simp]
+def SingleSortedOmegaCategory.underlying (S : SingleSortedOmegaCategory obj) (m : ℕ) :
+    SingleSortedNCategory m (cells m obj) where
+  sc k f := ⟨S.sc k f, by exact S.sck_scj_eq_scj f k.isLt⟩
+  tg k f := ⟨S.tg k f, by
+    apply (cell_sc_iff_cell_tg m _).mpr
+    exact S.tgk_tgj_eq_tgj f k.isLt⟩
+  pcomp k g f :=
+  {
+    Dom := (S.pcomp k g f).Dom
+    get dom :=
+      let comp_gf := S.comp k g f (S.pcomp_dom.mp dom)
+      ⟨comp_gf, by
+        simp
+        calc
+          _
+          _ = _ := S.sck_compj_eq_compj_sck k.isLt (S.pcomp_dom.mp dom)
+          _ = comp_gf := by apply congr_comp₁ f.property g.property⟩
+  }
+  pcomp_dom := by inherit_axiom S.pcomp_dom
   sck_sck_eq_sck := by inherit_axiom S.sck_sck_eq_sck
   tgk_sck_eq_sck := by inherit_axiom S.tgk_sck_eq_sck
   sck_tgk_eq_tgk := by inherit_axiom S.sck_tgk_eq_tgk
@@ -74,14 +117,36 @@ def SingleSortedNCategory.underlying (S : SingleSortedNCategory n obj) (m : Fin 
   exchange := by inherit_axiom S.exchange
 
 variable {C : Type u} {D : Type v} [SC : SingleSortedNCategory n C] [SD : SingleSortedNCategory n D]
+  [ωSC : SingleSortedOmegaCategory C] [ωSD : SingleSortedOmegaCategory D]
 
 /-- TODO: Comment. -/
+@[simp]
 def SingleSortedNFunctor.underlying (F : SingleSortedNFunctor n C D) (m : Fin n) :
     letI := SC.underlying m
     letI := SD.underlying m
     SingleSortedNFunctor m (cells m C) (cells m D) :=
   let uSC := SC.underlying m
   let uSD := SD.underlying m
+  {
+    map f := ⟨F f, by
+      simp
+      calc
+        sc m (F f)
+        _ = F (sc m f) := (F.map_sc_eq_sc_map m f).symm
+        _ = F f := by rw [f.property]⟩
+    map_sc_eq_sc_map := by inherit_axiom F.map_sc_eq_sc_map
+    map_tg_eq_tg_map := by inherit_axiom F.map_tg_eq_tg_map
+    map_comp_eq_comp_map := by inherit_axiom F.map_comp_eq_comp_map
+  }
+
+/-- TODO: Comment. -/
+@[simp]
+def SingleSortedOmegaFunctor.underlying (F : SingleSortedOmegaFunctor C D) (m : ℕ) :
+    letI := ωSC.underlying m
+    letI := ωSD.underlying m
+    SingleSortedNFunctor m (cells m C) (cells m D) :=
+  let uSC := ωSC.underlying m
+  let uSD := ωSD.underlying m
   {
     map f := ⟨F f, by
       simp
