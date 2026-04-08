d8e04e881e2d507842576a10d7a5dadde7347943
mariovagomarzal <mariovagomarzal@gmail.com>
2026-04-08 20:20:55 +0200

    refactor: upgrade the `ENat` import in the indices module
    The import is changed from `ENat.Defs` to `ENat.Basic`, which
    re-exports the former and additionally provides the order instances
    for `ENat` needed downstream.

--- HigherCategoryTheory/Indices.lean
+++ HigherCategoryTheory/Indices.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Enric Cosme Llópez, Raul Ruiz Mora, Mario Vago Marzal
 -/
 import Mathlib.Order.Basic
-import Mathlib.Data.ENat.Defs
+import Mathlib.Data.ENat.Basic
 import Mathlib.Data.Nat.Basic
 
 /-!
